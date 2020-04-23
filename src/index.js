33b8086ea2f7761f9dce8bc9b622f2b71c80223f
David Johnston <djohnston@whispir.com>
2020-04-23 10:12:17 +1000

    Add everything

---
+++ src/index.js
@@ -0,0 +1,3 @@
+export {default as CheckboxList} from "./assets/CheckboxList.svg";
+export { default as ChevronLeft } from './assets/ChevronLeft.svg';
+export { default as ChevronRight } from './assets/ChevronRight.svg';
