980b68053b1c343bcb9ca683abe5901f905d8b76
David Johnston <djohnston@whispir.com>
2020-04-23 16:29:51 +1000

    Whoops, update everythign

--- src/0index.js
+++ src/0index.js
@@ -1,9 +1,4 @@
 
-import  CheckboxList  from './assets/CheckboxList.svg';
-export const Icons = {
-    CheckboxList
-};
-
-// export {default as CheckboxList} from "./assets/CheckboxList.svg";
-// export { default as ChevronLeft } from './assets/ChevronLeft.svg';
-// export { default as ChevronRight } from './assets/ChevronRight.svg';
+export {default as CheckboxList} from "./assets/CheckboxList.svg";
+export { default as ChevronLeft } from './assets/ChevronLeft.svg';
+export { default as ChevronRight } from './assets/ChevronRight.svg';
